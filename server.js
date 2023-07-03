cd1f9986ebf04a701f0a2d628effeadf2e055550
AyanaZaire <ayanazaire@gmail.com>
2023-07-03 12:35:07 -0400

    poem schema, model, get + post routes

--- server.js
+++ server.js
@@ -19,3 +19,49 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
+
+// PART 3
+// Load CSS and JS File
+app.use(express.static('public'))
+
+// Serve HTML File 
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/views/index.html')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Your app is listening on port: ${port}`)
+})
+
+const PoemSchema = new mongoose.Schema({
+    title: {type: String},
+    author: {type: String},
+    poem: {type: String},
+    source: {type: String},
+    book: {type: String},
+    publisher: {type: String},
+    year: {type: Number},
+    images: {type: [String]}
+  });
+
+const Poem = mongoose.model("Poem", PoemSchema)
+
+app.get('/poems', async (request, response) => {
+    const poems = await Poem.find({})
+    try {
+      response.send(poems);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+});
+
+app.post("/poems", async (request, response) => {
+    const poem = new Poem(request.body);
+    try {
+      await poem.save();
+      response.send(poem);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+});
