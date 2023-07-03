82cd1343b5dab00eead6686a4181989aa2e3dccf
AyanaZaire <ayanazaire@gmail.com>
2023-07-03 11:31:20 -0400

    established database connection

--- server.js
+++ server.js
@@ -2,3 +2,20 @@
 const express = require('express')
 const app = express()
 app.use(express.json());
+
+// PART 2
+require("dotenv").config();
+
+// Testing URI eviornment variable access
+console.log(process.env.MONGO_URI)
+
+// Set up Mongoose
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Test for seccuess database connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
